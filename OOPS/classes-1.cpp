7208eee430aa4d0f371bdd1bd8d33a7665904d0f
Hritika Agarwal <hritikaa9@gmail.com>
2022-01-31 13:48:18 +0530

    Classes-1

---
+++ OOPS/classes-1.cpp
@@ -0,0 +1,49 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+class student{
+    public:
+    string name;
+    int age;
+    bool gender;
+    
+    void printInfo(){
+        cout<<"Name - ";
+        cout<<name<<endl;
+        cout<<"Age - ";
+        cout<<age<<endl;
+        cout<<"Gender - ";
+        cout<<gender<<endl;
+        
+    }
+};
+
+int main()
+{   
+    cout<<"Enter the no. of records you want to enter: ";
+    int n;
+    cin>>n;
+    student arr[n];
+    for(int i =0;i<n;i++){
+        cout<<"Name: ";
+        cin>>arr[i].name;
+        cout<<"Age: ";
+        cin>>arr[i].age;
+        cout<<"gender: ";
+        cin>>arr[i].gender;
+    }
+  
+  for(int i=0;i<n;i++){
+      arr[i].printInfo();
+  }
+    return 0;
+}
