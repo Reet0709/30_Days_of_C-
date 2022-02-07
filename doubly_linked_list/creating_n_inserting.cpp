879d533e86f38e80a1d45e185c5b861c4737e740
Hritika Agarwal <hritikaa9@gmail.com>
2022-02-07 17:49:16 +0530

    Doubly Linked List

---
+++ doubly_linked_list/creating_n_inserting.cpp
@@ -0,0 +1,71 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+class node {
+    public:
+    int data;
+    node* next;
+    node* previous;
+    
+    node(int val){
+        data = val;
+        next = NULL;
+        previous = NULL;
+    }
+};
+
+void insertAtHead(node* &head, int value){
+    node* n = new node(value);
+    n->next = head;
+    if (head!=NULL){
+        head->previous = n;
+    }
+    head = n;
+}
+
+void insertAtTail(node* &head, int value){
+    
+    if(head ==NULL){
+        insertAtHead(head,value);
+        return;
+    }
+    node* n = new node(value);
+    node* temp = head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next = n;
+    n->previous = temp;
+    n->next = NULL;
+}
+
+
+void display(node* &head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp= temp->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    node* head = NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    insertAtHead(head,5);
+    display(head);
+
+    return 0;
+}
